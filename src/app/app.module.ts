f022439dc2e05aefc8c41cb4ccfcc7d5c434c495
Alice Guan <aliguan726@gmail.com>
2017-07-01 12:18:52 -0400

    Authentication through Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ServiceSession } from './session.service';
+import { SessionService } from './session.service';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +10,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [ServiceSession],
+  providers: [SessionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
